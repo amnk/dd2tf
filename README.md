bcaac1045338a9e7d1fa96659d96c4067b0124d3
Aleksandr <alatar325@gmail.com>
2017-11-27 13:52:15 +0100

    Lots of improvements, including:
    - it is now possible to render all dashboards, or selected ones;
    - switch to "text/template";
    - support of markers;
    - support of styles;
    - some attempts to reduce number of newlines;

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-A simple utility to convert DataDog Dashboard to Terraform format.
+A simple utility to convert DataDog Dashboard to Terraform format. 
+
+Requires `DATADOG_API_KEY` and `DATADOG_APP_KEY` environment variables.
 
 Useful, if you had all dashboards configured adhoc and now want to follow DevOps style :)
--- main.go
+++ main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
-    "html/template"
+    "text/template"
+    "flag"
     "log"
     "os"
     "strconv"
+    "strings"
     "github.com/zorkian/go-datadog-api"
 )
+
+func getAllDashboards(client datadog.Client) []int {
+    var ids []int
+    dash_ids, _ := client.GetDashboards()
+    for _, elem := range dash_ids {
+        ids = append(ids, *elem.Id)
+    }
+    return ids
+}
 
 func main() {
     datadog_api_key, ok := os.LookupEnv("DATADOG_API_KEY")
@@ -19,21 +30,30 @@ func main() {
         log.Fatalf("Datadog APP key not found, please make sure that DATADOG_APP_KEY env variable is set")
     }
 
-    datadog_dashboard, ok := os.LookupEnv("DATADOG_DASH_ID")
-    if !ok {
-        log.Fatalf("Datadog dashboard id not found, please make sure that DATADOG_DASH_ID env variable is set")
-    }
+    var dashboards = flag.String("dashboards", "",
+        "IDs of dashboards, separated by comma. If nothing is given, all dashboards are parsed")
+    flag.Parse()
 
     client := datadog.NewClient(datadog_api_key, datadog_app_key)
 
-    dash_id, err := strconv.Atoi(datadog_dashboard)
-
-    dash, err := client.GetDashboard(dash_id)
-    if err != nil {
-        log.Fatalf("fatal: %s\n", err)
+    var dash_ids []int
+    if len(*dashboards) == 0 {
+        dash_ids = getAllDashboards(*client)
+    } else {
+        for _, element := range strings.Split(*dashboards, ",") {
+            dash, _ := strconv.Atoi(element)
+            dash_ids = append(dash_ids, dash)
+        }
     }
 
-    t := template.New("timeboard.tmpl")
-    t, _ = t.ParseFiles("timeboard.tmpl")
-    t.Execute(os.Stdout, *dash)
+    for _, element := range dash_ids {
+        dash, err := client.GetDashboard(element)
+        if err != nil {
+            log.Fatalf("fatal: %s\n", err)
+        }
+
+        t := template.New("timeboard.tmpl").Funcs(template.FuncMap{"StringsJoin": strings.Join})
+        t, _ = t.ParseFiles("timeboard.tmpl")
+        t.Execute(os.Stdout, *dash)
+    }
 }
